e79fcc30055a50309798bb17f12e7b090004ebad
fahadfarooqmurawat <fahadfarooqmurawat@gmail.com>
2023-01-06 04:58:56 +0500

    rule verification now uses sort order but ignores the comments

--- tests/lib/rules/comment-and-sort.js
+++ tests/lib/rules/comment-and-sort.js
@@ -9,66 +9,84 @@ const ruleTester = new RuleTester();
 
 ruleTester.run("comment-and-sort", rule, {
   valid: [
-    // {
-    //   code: "const button = styled.button`height: 200px; width: 300px;`",
-    //   parserOptions
-    // },
-    // {
-    //   code: "const button = styled(Button)`height: 200px; width: 300px;`",
-    //   parserOptions
-    // },
-    // {
-    //   code: "const button = css`height: 200px; width: 300px;`",
-    //   parserOptions
-    // },
-    // {
-    //   code: `const button = styled.button\`
-    //      height: 200px;
-    //      width: 300px;\``,
-    //   parserOptions
-    // },
-    // {
-    //   code: `const button = styled.button.attrs({ someAttribute: "unknown" })\`
-    //      height: 200px;
-    //      width: 300px;\``,
-    //   parserOptions,
-    // },
-    // {
-    //   code: `const button = styled(Button).attrs({ someAttribute: "unknown" })\`height: 200px; width: 300px;\``,
-    //   parserOptions,
-    // },
-    // {
-    //   code: `const button = styled.button\`
-    //      color: \${({ isBlue }) => (isBlue ? "blue" : "red")};
-    //      height: 200px;
-    //      width: 300px;\``,
-    //   parserOptions
-    // },
-    // {
-    //   code: `const button = styled.button\`
-    //      height: 200px;
-    //      stroke: \${Colors => Colors.selections};
-    //      width: 300px;\``,
-    //   parserOptions
-    // },
-    // {
-    //   code: `const button = styled.button\`
-    //      border: 1px solid
-    //        \${({ isBlue }) => (isBlue ? "blue" : "red")};
-    //      height: 200px;
-    //      width: 300px;\``,
-    //   parserOptions
-    // },
-    // {
-    //   code: `
-    //    const button = styled.button\`
-    //      border: 1px solid
-    //        \${({ isBlue }) => (isBlue ? "blue" : "red")};
-    //      width: 300px;
-    //      height: 200px;
-    //      left: 0;\``,
-    //   parserOptions
-    // }
+    {
+      code: "const button = styled.button`height: 200px; width: 300px;`",
+      parserOptions
+    },
+    {
+      code: "const button = styled(Button)`height: 200px; width: 300px;`",
+      parserOptions
+    },
+    {
+      code: "const button = css`height: 200px; width: 300px;`",
+      parserOptions
+    },
+    {
+      code: `const button = styled.button\`
+         height: 200px;
+         width: 300px;\``,
+      parserOptions
+    },
+    {
+      code: `const button = styled.button.attrs({ someAttribute: "unknown" })\`
+         height: 200px;
+         width: 300px;\``,
+      parserOptions,
+    },
+    {
+      code: `const button = styled(Button).attrs({ someAttribute: "unknown" })\`height: 200px; width: 300px;\``,
+      parserOptions,
+    },
+    {
+      code: `const button = styled.button\`
+         height: 200px;
+         width: 300px;
+         color: \${({ isBlue }) => (isBlue ? "blue" : "red")}\``,
+      parserOptions
+    },
+    {
+      code: `const button = styled.button\`
+         height: 200px;
+         width: 300px;
+         unknown: \${Colors => Colors.selections};\``,
+      parserOptions
+    },
+    {
+      code: `const button = styled.button\`
+         height: 200px;
+         width: 300px;
+         border: 1px solid
+           \${({ isBlue }) => (isBlue ? "blue" : "red")};\``,
+      parserOptions
+    },
+    {
+      code: `
+       const button = styled.button\`
+         left: 0;
+         height: 200px;
+         width: 300px;
+         border: 1px solid
+           \${({ isBlue }) => (isBlue ? "blue" : "red")};\``,
+      parserOptions
+    },
+    {
+      code: `
+       const button = styled.button\`
+         /* position */
+         position: absolute;
+         top: 0;
+         right: 0;
+         left: 0;
+
+         /* size */
+         height: 200px;
+         width: 300px;
+
+         /* appearance */
+         border: 1px solid
+           \${({ isBlue }) => (isBlue ? "blue" : "red")};\``,
+      parserOptions
+    }
   ],
 
   invalid: [
@@ -119,7 +137,7 @@ ruleTester.run("comment-and-sort", rule, {
         height: 200px;
         width: 300px;
         
-        /* misc */
+        /* appearance */
         background: yellow;
         background-color: red;\``,
     },
@@ -139,121 +157,121 @@ ruleTester.run("comment-and-sort", rule, {
          width: 300px;
          color: \${({ isBlue }) => (isBlue ? "blue" : "red")};\``
     },
-    // {
-    //   code: `const foo = styled.div\`
-    //      stroke: \${Colors => Colors.selections};
-    //      color: blue;
-    //    \`;`,
-    //   parserOptions,
-    //   errors: [
-    //     {
-    //       messageId: "comment-and-sort"
-    //     }
-    //   ],
-    //   output: `const foo = styled.div\`
-    //      color: blue;
-    //      stroke: \${Colors => Colors.selections};
-    //    \`;`
-    // },
-    // {
-    //   code: `const button = styled.button\`
-    //      width: 300px;
-    //      height: 200px;
-    //      border: 1px solid
-    //        \${({ isBlue }) => (isBlue ? "blue" : "red")};\``,
-    //   parserOptions,
-    //   errors: [
-    //     {
-    //       messageId: "comment-and-sort"
-    //     }
-    //   ],
-    //   output: `const button = styled.button\`
-    //      border: 1px solid
-    //        \${({ isBlue }) => (isBlue ? "blue" : "red")};
-    //      height: 200px;
-    //      width: 300px;\``
-    // },
-    // {
-    //   code: `
-    //    import styled from 'styled-components';
+    {
+      code: `const foo = styled.div\`
+         stroke: \${Colors => Colors.selections};
+         color: blue;
+       \`;`,
+      parserOptions,
+      errors: [
+        {
+          messageId: "comment-and-sort"
+        }
+      ],
+      output: `const foo = styled.div\`
+         color: blue;
+         stroke: \${Colors => Colors.selections};
+       \`;`
+    },
+    {
+      code: `const button = styled.button\`
+         width: 300px;
+         height: 200px;
+         border: 1px solid
+           \${({ isBlue }) => (isBlue ? "blue" : "red")};\``,
+      parserOptions,
+      errors: [
+        {
+          messageId: "comment-and-sort"
+        }
+      ],
+      output: `const button = styled.button\`
+         height: 200px;
+         width: 300px;
+         border: 1px solid
+           \${({ isBlue }) => (isBlue ? "blue" : "red")};\``
+    },
+    {
+      code: `
+       import styled from 'styled-components';
 
-    //    const button = styled.button\`
-    //      height: 200px;
-    //      border: 1px solid
-    //        \${({ isBlue }) => (isBlue ? "blue" : "red")};
-    //      width: 300px;\``,
-    //   parserOptions,
-    //   errors: [
-    //     {
-    //       messageId: "comment-and-sort"
-    //     }
-    //   ],
-    //   output: `
-    //    import styled from 'styled-components';
+       const button = styled.button\`
+         height: 200px;
+         border: 1px solid
+           \${({ isBlue }) => (isBlue ? "blue" : "red")};
+         width: 300px;\``,
+      parserOptions,
+      errors: [
+        {
+          messageId: "comment-and-sort"
+        }
+      ],
+      output: `
+       import styled from 'styled-components';
 
-    //    const button = styled.button\`
-    //      border: 1px solid
-    //        \${({ isBlue }) => (isBlue ? "blue" : "red")};
-    //      height: 200px;
-    //      width: 300px;\``
-    // },
-    // {
-    //   code: `
-    //      export const foo = styled.div\`
-    //        height: 100%;
-    //        top: 0;
-    //        position: absolute;
-    //        width: 100%;
+       const button = styled.button\`
+         height: 200px;
+         width: 300px;
+         border: 1px solid
+           \${({ isBlue }) => (isBlue ? "blue" : "red")};\``
+    },
+    {
+      code: `
+         export const foo = styled.div\`
+           height: 100%;
+           top: 0;
+           position: absolute;
+           width: 100%;
 
-    //        .op-selectable:hover {
-    //          cursor: pointer;
-    //        }
-    //      \`;`,
-    //   parserOptions,
-    //   errors: [
-    //     {
-    //       messageId: "comment-and-sort"
-    //     }
-    //   ],
-    //   output: `
-    //      export const foo = styled.div\`
-    //        height: 100%;
-    //        position: absolute;
-    //        top: 0;
-    //        width: 100%;
+           .op-selectable:hover {
+             cursor: pointer;
+           }
+         \`;`,
+      parserOptions,
+      errors: [
+        {
+          messageId: "comment-and-sort"
+        }
+      ],
+      output: `
+         export const foo = styled.div\`
+           position: absolute;
+           top: 0;
+           height: 100%;
+           width: 100%;
 
-    //        .op-selectable:hover {
-    //          cursor: pointer;
-    //        }
-    //      \`;`
-    // },
-    // {
-    //   code: `
-    //      export const foo = styled.div\`
-    //        flex-grow: 1;
-    //        .op-selected .djs-outline {
-    //          stroke-width: 3px;
-    //          fill: \${themeStyle({
-    //            dark: "rgba(58, 82, 125, 0.5)",
-    //            light: "rgba(189, 212, 253, 0.5)"
-    //          })} !important;
-    //        }\`;`,
-    //   parserOptions,
-    //   errors: [
-    //     {
-    //       messageId: "comment-and-sort"
-    //     }
-    //   ],
-    //   output: `
-    //      export const foo = styled.div\`
-    //        flex-grow: 1;
-    //        .op-selected .djs-outline {
-    //          fill: \${themeStyle({
-    //            dark: "rgba(58, 82, 125, 0.5)",
-    //            light: "rgba(189, 212, 253, 0.5)"
-    //          })} !important;
-    //          stroke-width: 3px;
-    //        }\`;`
-    // }
+           .op-selectable:hover {
+             cursor: pointer;
+           }
+         \`;`
+    },
+    {
+      code: `
+         export const foo = styled.div\`
+           flex-grow: 1;
+           .op-selected .djs-outline {
+             stroke-width: 3px;
+             fill: \${themeStyle({
+               dark: "rgba(58, 82, 125, 0.5)",
+               light: "rgba(189, 212, 253, 0.5)"
+             })} !important;
+           }\`;`,
+      parserOptions,
+      errors: [
+        {
+          messageId: "comment-and-sort"
+        }
+      ],
+      output: `
+         export const foo = styled.div\`
+           flex-grow: 1;
+           .op-selected .djs-outline {
+             fill: \${themeStyle({
+               dark: "rgba(58, 82, 125, 0.5)",
+               light: "rgba(189, 212, 253, 0.5)"
+             })} !important;
+             stroke-width: 3px;
+           }\`;`
+    }
   ],
 });
---
+++ utils/categories-map.js
@@ -0,0 +1,47 @@
+module.exports = {
+  position: "position",
+  inset: "position",
+  top: "position",
+  right: "position",
+  bottom: "position",
+  left: "position",
+
+  display: "display",
+  flex: "display",
+  "flex-grow": "display",
+
+  height: "size",
+  width: "size",
+  margin: "size",
+  "margin-top": "size",
+  "margin-right": "size",
+  "margin-bottom": "size",
+  "margin-left": "size",
+  padding: "size",
+  "padding-top": "size",
+  "padding-right": "size",
+  "padding-bottom": "size",
+  "padding-left": "size",
+
+  background: "appearance",
+  "background-color": "appearance",
+  color: "appearance",
+  border: "appearance",
+  "border-radius": "appearance",
+  transform: "appearance",
+  fill: "appearance",
+  "stroke-width": "appearance",
+  
+  cursor: "misc",
+  "pointer-events": "misc",
+  
+  "font-family": "font",
+  "font-size": "font",
+  "font-style": "font",
+  "font-weight": "font",
+  "line-height": "font",
+  "text-align": "font",
+  "text-transform": "font",
+
+  transition: "animation"
+};
